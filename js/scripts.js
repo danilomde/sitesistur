0748e11e8b8c3d41710441db1e32db67400d5041
Artur <arturcedaspy@gmail.com>
2019-05-01 11:44:11 -0300

    FormularioModal_VideoReal

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,8 @@
-$(document).ready(function(){
-    $window = $(window);
-    //Captura cada elemento section com o data-type "background"
-    $('section[data-type="background"]').each(function(){
-        var $scroll = $(this);   
-            //Captura o evento scroll do navegador e modifica o backgroundPosition de acordo com seu deslocamento.            
-            $(window).scroll(function() {
-                var yPos = -($window.scrollTop() / $scroll.data('speed')); 
-                var coords = '50% '+ yPos + 'px';
-                $scroll.css({ backgroundPosition: coords });    
-            });
-   });  
-}); 
+function teste() {
+    alert("Parabéns!!! Você atingiu os requisitos mínimos para participar do programa.\n\n Dê print dessa tela e vá a unidade JUMPER! mais próxima ou nos encaminhe via WhatsApp e aguarde próximas orientações!");
+
+    var telefone = '5541999852493';
+    var msg = 'Olá! Passei nos requisitos para participar do programa Intertour.';
+
+   // window.open('https://api.whatsapp.com/send?phone='+telefone+'&text='+msg);
+}
