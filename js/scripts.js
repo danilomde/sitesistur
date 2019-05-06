ae34868c6cbacda0e7221a4fbf2208c236296207
Artur <arturcedaspy@gmail.com>
2019-05-06 10:56:03 -0300

    Final

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,35 @@
-function teste() {
-    alert("Parabéns!!! Você atingiu os requisitos mínimos para participar do programa.\n\n Dê print dessa tela e vá a unidade JUMPER! mais próxima ou nos encaminhe via WhatsApp e aguarde próximas orientações!");
+function redirecionando() {
+    
 
-    var telefone = '5541999852493';
-    var msg = 'Olá! Passei nos requisitos para participar do programa Intertour.';
+    // var telefone = '5541999852493';
+    // var msg = 'Olá! Passei nos requisitos para participar do programa Intertour.';
 
-   // window.open('https://api.whatsapp.com/send?phone='+telefone+'&text='+msg);
+    // window.open('https://api.whatsapp.com/send?phone='+telefone+'&text='+msg);
+
+	
 }
+
+function validar() {
+		var nome = formTeste.nome.value;
+		var celular = formTeste.celular.value;
+		var telefone = '5541999852493';
+		var msg = 'Olá, meu nome é '+nome+'! Eu Passei nos requisitos mínimos para participar do programa Intertour, como devo proceder agora?';
+
+		
+
+		if (nome == "") {
+			document.getElementById("erro_nome").innerHTML = "<span class='textError'>Preencha com um nome!</span>";
+			formTeste.nome.focus();
+			return false;
+		}else
+			if (celular == "") {
+				document.getElementById("erro_celular").innerHTML = "<span class='textError'>Preencha com um celular!</span>";
+				formTeste.nome.focus();
+				return false;
+			}else{
+				alert("Parabéns "+nome+"!!! Você atingiu os requisitos mínimos para participar do programa.");
+
+   				window.open('https://api.whatsapp.com/send?phone='+telefone+'&text='+msg);
+			}
+
+	}
